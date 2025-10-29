38ce96b00c15874dc8b6362b8e11cd0ceef6dd11
mateo vargas <mateo.vargast@udea.edu.co>
2025-10-29 11:19:42 -0500

    Mejora de GUI

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # main.py
+from fontTools.misc.cython import returns
+
 from utils.detect_plate import detect_plate
 from utils.segment_characters import segment_characters
 from features.extract_features import extract_hog_features
@@ -7,13 +9,12 @@ import cv2
 import os
 import numpy as np
 
-MODEL_PATH = "models/saved_models/hog_svm.pkl"
 
 def predict_characters(plate_path, method, char_paths):
+    recognized = ""
 
-    if method.upper() == "hog":
+    if method.upper() == "HOG":
         model, le = joblib.load("models/saved_models/hog_svm.pkl")
-        recognized = ""
 
         for c_path in char_paths:
             img = cv2.imread(c_path, cv2.IMREAD_GRAYSCALE)
@@ -27,27 +28,14 @@ def predict_characters(plate_path, method, char_paths):
 
         return recognized
 
+    elif method.upper() == "LBP":
+        print("Selecciono LBP")
 
-    char_paths = segment_characters(plate_path)
-    model, le = joblib.load(MODEL_PATH)
-
-    recognized = ""
-    for c_path in char_paths:
-        img = cv2.imread(c_path, cv2.IMREAD_GRAYSCALE)
-        img = cv2.resize(img, (64, 64))
-        feat = extract_hog_features([img])
-        pred = model.predict(feat)
-        label = le.inverse_transform(pred)[0]
-        recognized += clean_label(label)
-
-    print(f"✅ Matrícula detectada: {recognized}")
+        return "LBP"
+    print ("No se seleccionono metodo")
     return recognized
 
 def clean_label(label: str) -> str:
-    """
-    Limpia el texto devuelto por el modelo eliminando prefijos como 'class_'.
-    Ejemplo: 'class_A' -> 'A'
-    """
     if isinstance(label, str) and label.startswith("class_"):
         return label.replace("class_", "")
     return label
